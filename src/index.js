8b32accfcb2cc7997fc0f39c7968ff9577ee4e5b
Kristina Diaz <kristina.diaz84@gmail.com>
2021-11-12 12:00:56 -0700

    Code Challenge Part 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-// write your code here
+const imgUrl = 'http://localhost:3000/images/1';
+
+// GET request to server
+fetch(imgUrl)
+.then(res => res.json())
+.then(flataPage)
+
+// Render image on page
+function flataPage(picture) {
+    const imageGram = document.querySelector('.image-container');
+
+    const pic = document.querySelector('.title');
+    pic.textContent = picture.title;
+    
+    const img = document.querySelector('#card-image');
+    img.src = picture.image;
+    img.alt = picture.title;  
+};
+
+// add comments to ul#comments-list, will need to remove existing list
+    // iterate through the comments array .forEach()?
+
+// Event listener to click like button, it will increase the likes by 1
+   // Will need to target picture.likes
+const button = document.querySelector('#like-button');
+button.addEventListener('click', e => {
+    e.preventDefault();
+
+    console.log(e.target['like-button'])
+});
+
+// Event listener to submit a new comment
+const newComment = document.querySelector('#comment-form');
+newComment.addEventListener('submit', e => {
+    e.preventDefault();
+
+    console.log(e.target['comment-button'])
+});
